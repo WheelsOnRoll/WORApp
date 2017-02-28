ae8b780feb0c4d8ea82f81d16df5f1550e714a47
xSantosh42x <santoshkvk@gmail.com>
2017-02-28 23:04:12 +0530

    adding spalsh

--- WOR/app/src/main/java/wor/com/wor/SplashScreenActivity.java
+++ WOR/app/src/main/java/wor/com/wor/SplashScreenActivity.java
@@ -6,6 +6,8 @@ import android.graphics.PorterDuff;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -17,10 +19,17 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_axtivity);
         ((ProgressBar)findViewById(R.id.splash_loader)).getIndeterminateDrawable().setColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY);
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
+        }
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
             }
         }, SPLASH_DELAY);
     }
