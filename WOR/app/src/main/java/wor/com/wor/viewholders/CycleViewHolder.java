1aa5981ddf4b15c9f81747f1ddc457376f05bab8
salman-bhai <sbs.191197@gmail.com>
2017-03-18 15:59:17 +0530

    Front end changes made

--- WOR/app/src/main/java/wor/com/wor/viewholders/CycleViewHolder.java
+++ WOR/app/src/main/java/wor/com/wor/viewholders/CycleViewHolder.java
@@ -10,10 +10,6 @@ import android.widget.TextView;
 import wor.com.wor.R;
 import wor.com.wor.models.Cycle;
 
-/**
- * Created by Santosh on 14-Jan-17.
- */
-
 public class CycleViewHolder extends RecyclerView.ViewHolder{
 
     public ImageView cycleimage;
